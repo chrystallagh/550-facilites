9f42c8426eb71d30ffed19b5dbe15b0d71db4a20
chrystallagh <17602712+chrystallagh@users.noreply.github.com>
2022-04-19 15:26:17 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+# !pip3 install flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def render_the_map():
+    return render_template('facilities_map.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
